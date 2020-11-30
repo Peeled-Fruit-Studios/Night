b06f54926ae27fabe1b23b3f706b0c3a2b47c0bc
Uiop Pilon <circutrider21@gmail.com>
2020-11-30 02:12:04 +0000

    Fixing build Mistake :-(

--- kernel/src/kernel/start.s
+++ kernel/src/kernel/start.s
@@ -1,46 +1,29 @@
-; bkerndev - Bran's Kernel Development Tutorial
-; By:   Brandon F. (friesenb@gmail.com)
-; Desc: Kernel entry point, stack, and Interrupt Service Routines.
-;
-; Notes: No warranty expressed or implied. Use at own risk.
-;
-; This is the kernel's entry point. We could either call main here,
-; or we can use this to setup the stack or other nice stuff, like
-; perhaps setting up the GDT and segments. Please note that interrupts
-; are disabled at this point: More on interrupts later!
 [BITS 32]
 global _start
 _start:
-    mov esp, _sys_stack     ; This points the stack to our new stack area
+    mov esp, _sys_stack     
     jmp stublet
 
-; This part MUST be 4byte aligned, so we solve that issue using 'ALIGN 4'
-ALIGN 4
 [section .multiboot]
-    ; Multiboot macros to make a few lines later more readable
-    MULTIBOOT_PAGE_ALIGN	equ 1<<0
-    MULTIBOOT_MEMORY_INFO	equ 1<<1
-    MULTIBOOT_HEADER_MAGIC	equ 0x1BADB002
-    MULTIBOOT_HEADER_FLAGS	equ MULTIBOOT_PAGE_ALIGN | MULTIBOOT_MEMORY_INFO
-    MULTIBOOT_CHECKSUM	equ -(MULTIBOOT_HEADER_MAGIC + MULTIBOOT_HEADER_FLAGS)
+  ; Multiboot macros to make a few lines later more readable
+  MULTIBOOT_PAGE_ALIGN	equ 1<<0
+  MULTIBOOT_MEMORY_INFO	equ 1<<1
+  MULTIBOOT_HEADER_MAGIC	equ 0x1BADB002
+  MULTIBOOT_HEADER_FLAGS	equ MULTIBOOT_PAGE_ALIGN | MULTIBOOT_MEMORY_INFO
+  MULTIBOOT_CHECKSUM	equ -(MULTIBOOT_HEADER_MAGIC + MULTIBOOT_HEADER_FLAGS)
 
-    ; This is the GRUB Multiboot header. A boot signature
-    dd MULTIBOOT_HEADER_MAGIC
-    dd MULTIBOOT_HEADER_FLAGS
-    dd MULTIBOOT_CHECKSUM
+  ; This is the GRUB Multiboot header. A boot signature
+  dd MULTIBOOT_HEADER_MAGIC
+  dd MULTIBOOT_HEADER_FLAGS
+  dd MULTIBOOT_CHECKSUM
 
-; This is an endless loop here. Make a note of this: Later on, we
-; will insert an 'extern _main', followed by 'call _main', right
-; before the 'jmp $'.
+
 stublet:
     extern main
     call main
     jmp $
 
-; This will set up our new segment registers. We need to do
-; something special in order to set CS. We do what is called a
-; far jump. A jump that includes a segment as well as an offset.
-; This is declared in C as 'extern void gdt_flush();'
+
 global _gdt_flush
 extern gp
 _gdt_flush:
